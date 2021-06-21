3a87d32bc083bd1c4eaabcda4c52b15456ad09fe
Duyan Kim <46421950+duyankim@users.noreply.github.com>
2021-06-21 19:39:38 +0900

    [fix] `BoardItem` add some variables and constructor

--- src/main/java/kr/ac/kopo/domain/BoardItem.java
+++ src/main/java/kr/ac/kopo/domain/BoardItem.java
@@ -1,9 +1,36 @@
 package kr.ac.kopo.domain;
 
+import java.sql.Date;
+
 public class BoardItem {
-	private int id;
-	private String title;
-	private String content;
+	private int id, parent_id;
+	private Date date;
+	private String title, content, author;
+	private int relevel, viewcnt;
+	
+	public BoardItem() {
+		
+	}
+
+	public BoardItem(int parent_id, String title, String content, String author, int relevel, int viewcnt) {
+		this.parent_id = parent_id;
+		this.title = title;
+		this.content = content;
+		this.author = author;
+		this.relevel = relevel;
+		this.viewcnt = viewcnt;
+	}
+
+	public BoardItem(int id, Date date, int parent_id, String title, String content, String author, int relevel, int viewcnt) {
+		this.id = id;
+		this.date = date;
+		this.parent_id = parent_id;
+		this.title = title;
+		this.content = content;
+		this.author = author;
+		this.relevel = relevel;
+		this.viewcnt = viewcnt;
+	}
 
 	public int getId() {
 		return id;
@@ -11,6 +38,14 @@ public class BoardItem {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
 	}
 
 	public String getTitle() {
@@ -27,5 +62,37 @@ public class BoardItem {
 
 	public void setContent(String content) {
 		this.content = content;
+	}
+
+	public int getParent_id() {
+		return parent_id;
+	}
+
+	public void setParent_id(int parent_id) {
+		this.parent_id = parent_id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public int getRelevel() {
+		return relevel;
+	}
+
+	public void setRelevel(int relevel) {
+		this.relevel = relevel;
+	}
+
+	public int getViewcnt() {
+		return viewcnt;
+	}
+
+	public void setViewcnt(int viewcnt) {
+		this.viewcnt = viewcnt;
 	}
 }
