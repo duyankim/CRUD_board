aaa2cf00bb7ef5c1ef65a9c4b940d7366093965b
Duyan Kim <46421950+duyankim@users.noreply.github.com>
2021-06-22 20:54:37 +0900

    [fix] add method `newPostId` in `BoardItemDao`

--- src/main/java/kr/ac/kopo/dao/BoardItemDao.java
+++ src/main/java/kr/ac/kopo/dao/BoardItemDao.java
@@ -7,9 +7,11 @@ import kr.ac.kopo.domain.BoardItem;
 
 public interface BoardItemDao {
 	BoardItem create(BoardItem boardItem) throws Exception;
-	void update(BoardItem boardItem) throws Exception;
+	BoardItem update(BoardItem boardItem) throws Exception;
 	void delete(BoardItem boardItem) throws Exception;
-	Optional<BoardItem> selectOne(int id) throws Exception;
+	Optional<BoardItem> selectOne(int post_id) throws Exception;
 	List<BoardItem> selectAll(int board_id) throws Exception;
+	List<BoardItem> selectAll() throws Exception;
 	List<BoardItem> selectOnePageResult(int board_id, int page_num) throws Exception;
+	int newPostId() throws Exception;
 }
