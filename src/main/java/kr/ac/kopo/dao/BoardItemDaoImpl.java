79ddd93027a918477d4eecdf7190be5f81d6d091
Duyan Kim <46421950+duyankim@users.noreply.github.com>
2021-06-22 20:56:45 +0900

    [fix] `BoardItemDaoImpl` fix mysql query errors

--- src/main/java/kr/ac/kopo/dao/BoardItemDaoImpl.java
+++ src/main/java/kr/ac/kopo/dao/BoardItemDaoImpl.java
@@ -26,7 +26,7 @@ public class BoardItemDaoImpl implements BoardItemDao {
 	
 	@Override
 	public BoardItem create(BoardItem boardItem) {
-		String sql = "insert into board_item_table(date, parent_id, title, content, author, relevel, viewcnt) values(now(), ?, ?, ?, ?, ?, ?)";
+		String sql = "insert into board_item_table(date, parent_id, title, content, author) values(now(), ?, ?, ?, ?)";
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
@@ -37,17 +37,14 @@ public class BoardItemDaoImpl implements BoardItemDao {
 		try {
 			pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 			pstmt.setInt(1, boardItem.getParent_id());
-			pstmt.setString(2, boardItem.getContent());
-			pstmt.setString(3, boardItem.getTitle());
+			pstmt.setString(2, boardItem.getTitle());
+			pstmt.setString(3, boardItem.getContent());
 			pstmt.setString(4, boardItem.getAuthor());
-			pstmt.setInt(5, boardItem.getRelevel());
-			pstmt.setInt(6, boardItem.getViewcnt());
 			pstmt.executeUpdate();
 			rset = pstmt.getGeneratedKeys();
 
 			if (rset.next()) {
 				boardItem.setId(rset.getInt(1));
-				boardItem.setDate(rset.getDate(2));
 			} else {
 				throw new SQLException("id가 존재하지 않습니다.");
 			}
@@ -60,8 +57,8 @@ public class BoardItemDaoImpl implements BoardItemDao {
 	}
 
 	@Override
-	public Optional<BoardItem> selectOne(int id) {
-		String sql = "select * from board_item_table where id = ?";
+	public Optional<BoardItem> selectOne(int post_id) {
+		String sql = "select id, date, title, content, author, parent_id from board_item_table where id = ?";
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -72,13 +69,17 @@ public class BoardItemDaoImpl implements BoardItemDao {
 		
 		try {
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, id);
-			pstmt.executeQuery();
-			BoardItem boardItem;
+			pstmt.setInt(1, post_id);
+			rset = pstmt.executeQuery();
+			
+			BoardItem boardItem = new BoardItem();
 			if (rset.next()) {
-				boardItem = new BoardItem(
-						rset.getInt(1), rset.getDate(2), rset.getInt(3), rset.getString(4), 
-						rset.getString(5), rset.getString(6), rset.getInt(7), rset.getInt(8));
+				boardItem.setId(rset.getInt(1));
+				boardItem.setDate(rset.getDate(2));
+				boardItem.setTitle(rset.getString(3));
+				boardItem.setContent(rset.getString(4));
+				boardItem.setAuthor(rset.getString(5));
+				boardItem.setParent_id(rset.getInt(6));
 			} else {
 				throw new SQLException("게시글이 존재하지 않습니다.");
 			}
@@ -92,7 +93,7 @@ public class BoardItemDaoImpl implements BoardItemDao {
 
 	@Override
 	public List<BoardItem> selectAll(int board_id) {
-		String sql = "SELECT * FROM board_item_table i  INNER JOIN board_table b ON bi.parent_id = b.id and b.id = ?";
+		String sql = "SELECT * FROM board_item_table i INNER JOIN board_table b ON i.parent_id = b.id and b.id = ?";
 		
 		List<BoardItem> items = new ArrayList<BoardItem>();
 		BoardItem item = new BoardItem();
@@ -107,7 +108,44 @@ public class BoardItemDaoImpl implements BoardItemDao {
 		try {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, board_id);
-			pstmt.executeQuery();
+			rset = pstmt.executeQuery();
+			
+			while (rset.next()) {
+				item.setId(rset.getInt(1));
+				item.setDate(rset.getDate(2));
+				item.setContent(rset.getString(3));
+				item.setAuthor(rset.getString(4));
+				item.setParent_id(rset.getInt(5));
+				item.setRelevel(rset.getInt(6));
+				item.setViewcnt(rset.getInt(7));
+				item.setTitle(rset.getString(9));
+				items.add(item);
+			}
+			return items;
+		} catch (Exception e1) {
+			throw new IllegalStateException(e1);
+		} finally {
+			close(conn, pstmt, rset);
+		}
+	}
+	
+	@Override
+	public List<BoardItem> selectAll() {
+		String sql = "SELECT * FROM board_item_table";
+		
+		List<BoardItem> items = new ArrayList<BoardItem>();
+		BoardItem item = new BoardItem();
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rset = pstmt.executeQuery();
 			
 			while (rset.next()) {
 				item.setId(rset.getInt(1));
@@ -130,10 +168,7 @@ public class BoardItemDaoImpl implements BoardItemDao {
 	
 	@Override
 	public List<BoardItem> selectOnePageResult(int board_id, int page_num) {
-		String sql = "SELECT i.id, i.date, i.author, i.title "
-				+ "FROM board_item_table i  "
-				+ "INNER JOIN board_table b "
-				+ "ON i.parent_id = b.id and b.id = ? limit ?, ?";
+		String sql = "select id, date, author, title, content from board_item_table where parent_id=? limit ?, ?;";
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -145,25 +180,26 @@ public class BoardItemDaoImpl implements BoardItemDao {
 		List<BoardItem> items = new ArrayList<BoardItem>();
 		BoardItem item = new BoardItem();
 		int postNumPerPage = 5;
-		int firstPostIdOfThisPage = (page_num-1)*5+1;
+		int beginNumOfThisPage = (page_num-1)*5;
 
 		try {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, board_id);
-			pstmt.setInt(2, firstPostIdOfThisPage);
+			pstmt.setInt(2, beginNumOfThisPage);
 			pstmt.setInt(3, postNumPerPage);
-			pstmt.executeQuery();
+			rset = pstmt.executeQuery();
 
-			if (rset == null ) {    
-			    System.out.println("게시글이 없습니다."); 
-			} else {
+			if (rset != null) {
 				while (rset.next()) {				
 					item.setId(rset.getInt(1));
 					item.setDate(rset.getDate(2));
 					item.setAuthor(rset.getString(3));
 					item.setTitle(rset.getString(4));
+					item.setContent(rset.getString(5));
 					items.add(item);
 				}
+			} else {
+				 System.out.println("게시글이 없습니다."); 
 			}
 			return items;
 		} catch (Exception e1) {
@@ -174,7 +210,7 @@ public class BoardItemDaoImpl implements BoardItemDao {
 	}
 
 	@Override
-	public void update(BoardItem boardItem) {
+	public BoardItem update(BoardItem boardItem) {
 		String sql = "update board_item_table(title, content, author) set title = ?, content = ?, author = ? where id = ?";
 		
 		try {
@@ -191,6 +227,18 @@ public class BoardItemDaoImpl implements BoardItemDao {
 			pstmt.setString(3, boardItem.getAuthor());
 			pstmt.setInt(4, boardItem.getId());
 			pstmt.executeUpdate();
+			
+			BoardItem item = new BoardItem();
+			if (rset != null) {
+				while (rset.next()) {				
+					item.setId(rset.getInt(1));
+					item.setDate(rset.getDate(2));
+					item.setAuthor(rset.getString(3));
+					item.setTitle(rset.getString(4));
+					item.setContent(rset.getString(5));
+				}
+			}
+			return item;
 		} catch (Exception e1) {
 			throw new IllegalStateException(e1);
 		} finally {
@@ -213,6 +261,33 @@ public class BoardItemDaoImpl implements BoardItemDao {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, boardItem.getId());
 			pstmt.executeUpdate();
+		} catch (Exception e1) {
+			throw new IllegalStateException(e1);
+		} finally {
+			close(conn, pstmt, rset);
+		}
+	}
+
+	@Override
+	public int newPostId() throws Exception {
+		String sql = "select * from board_item_table";
+		int result=0;
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rset = pstmt.executeQuery();
+			
+			while (rset.next()) {
+				result = rset.getInt(1);
+			}
+			return result+1;
 		} catch (Exception e1) {
 			throw new IllegalStateException(e1);
 		} finally {
