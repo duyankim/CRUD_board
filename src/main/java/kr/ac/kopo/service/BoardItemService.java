1e541f5d9dbb1858b56ab8e012c6f8554347da32
Duyan Kim <46421950+duyankim@users.noreply.github.com>
2021-06-23 23:35:06 +0900

    [refactor] fix parameter of `delete` method

--- src/main/java/kr/ac/kopo/service/BoardItemService.java
+++ src/main/java/kr/ac/kopo/service/BoardItemService.java
@@ -5,14 +5,15 @@ import java.util.*;
 import kr.ac.kopo.domain.BoardItem;
 
 public interface BoardItemService {
-	BoardItem create(String author, String title, String content) throws Exception;
+	BoardItem create(String author, String title, String content, int board_id) throws Exception;
 	BoardItem create(BoardItem boardItem) throws Exception;
 	BoardItem update(String author, String title, String content) throws Exception;
-	void delete(BoardItem boardItem) throws Exception;
+	void delete(int post_id) throws Exception;
 	BoardItem viewOne(int post_id) throws Exception;
 	List<BoardItem> viewAll(int board_id) throws Exception;
 	List<BoardItem> viewAll() throws Exception;
 	List<BoardItem> viewOnePageResult(int board_id, int page_num) throws Exception;
 	String getCurrentDate() throws Exception;
 	int newPostId() throws Exception;
+	int findBoardIdByPostId(int post_id) throws Exception;
 }
