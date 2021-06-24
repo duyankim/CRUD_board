1d9d05ad600e58becd70377b41daaff423b1d139
Duyan Kim <46421950+duyankim@users.noreply.github.com>
2021-06-24 23:40:56 +0900

    [feat] `BoardItemServiceImpl.java` add viewAllComment method and overload create method

--- src/main/java/kr/ac/kopo/service/BoardItemServiceImpl.java
+++ src/main/java/kr/ac/kopo/service/BoardItemServiceImpl.java
@@ -2,9 +2,11 @@ package kr.ac.kopo.service;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import kr.ac.kopo.dao.*;
+import kr.ac.kopo.dao.BoardItemDaoImpl;
 import kr.ac.kopo.domain.BoardItem;
 
 public class BoardItemServiceImpl implements BoardItemService {
@@ -26,6 +28,12 @@ public class BoardItemServiceImpl implements BoardItemService {
 	public BoardItem create(String author, String title, String content, int board_id) {
 		BoardItem boardItem = new BoardItem(author, title, content, board_id);
 		return dao.create(boardItem);
+	}
+
+	@Override
+	public BoardItem create(int parent_id, int board_id, String author, String content, int relevel){
+		BoardItem boardItem = new BoardItem(parent_id, board_id, content, author, relevel);
+		return dao.createComment(boardItem);
 	}
 	
 	@Override
@@ -79,5 +87,21 @@ public class BoardItemServiceImpl implements BoardItemService {
 	@Override
 	public int findBoardIdByPostId(int post_id) throws Exception {
 		return dao.findBoardIdByPostId(post_id);
+	}
+
+	@Override
+	public List<BoardItem> viewAllComment(int parent_id) throws Exception {
+		return dao.selectAllComment(parent_id);
+	}
+	
+	public List<BoardItem> viewRelevelComments(int relevel, int post_id) {
+		List<BoardItem> comments = null;
+		try {
+			comments = viewAllComment(post_id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		List<BoardItem> relevelComments = comments.stream().filter(c -> c.getRelevel() == 1).collect(Collectors.toList());
+		return relevelComments;
 	}
 }
